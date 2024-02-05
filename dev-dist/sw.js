4766c1a11bb43802b555e3f629a1dc8e35fcd5cb
Benjamin <benjamindeltour22@gmail.com>
2024-02-05 14:13:06 +0100

    implement the router

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.lkarf8q2l18"
+    "revision": "0.uehaqde652g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
