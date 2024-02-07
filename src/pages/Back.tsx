2a1b91dd76b33147e0bbdf78a787ab3ca6ca1019
Benjamin <benjamindeltour22@gmail.com>
2024-02-07 13:37:22 +0100

    insert all the cinema

--- src/pages/Back.tsx
+++ src/pages/Back.tsx
@@ -1,3 +1,5 @@
+import data from '../data/etablissements-cinematographiques.json'
+
 async function fetchData() {
     try {
         const response = await fetch('http://localhost:3000/api/data');
@@ -5,7 +7,7 @@ async function fetchData() {
             throw new Error('Failed to fetch data');
         }
         const data = await response.json();
-        console.log(data);
+        console.log(data)
     } catch (error) {
         console.error('Error fetching data:', error);
     }
