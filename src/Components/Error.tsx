09905bb9b64a7d09d5678dac4c5397e333103329
Benjamin <benjamindeltour22@gmail.com>
2024-02-28 09:58:06 +0100

    change how we declare the error prop type

--- src/Components/Error.tsx
+++ src/Components/Error.tsx
@@ -2,7 +2,7 @@ interface ErrorProps {
   erreur: string;
 }
 
-const Error: React.FC<ErrorProps> = ({ erreur }) => {
+const Error = ({ erreur }: ErrorProps) => {
   return (
     <div className="flex items-center justify-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
       <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
